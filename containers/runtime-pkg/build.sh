0452a9171415c3827240ee648b375e32e550f736
Frans-Jan van Steenbeek <frans-jan@van-steenbeek.net>
2025-06-02 17:39:11 +0200

    Add FreeBSD-rc, some pkgs fail to install cleanly

--- containers/runtime-pkg/build.sh
+++ containers/runtime-pkg/build.sh
@@ -6,5 +6,5 @@ mkdir -p "${rootdir}usr/local/etc/pkg/repos"
 cp -r /usr/share/keys/pkg/trusted "${rootdir}usr/share/keys/pkg/"
 cp /etc/pkg/*.conf "${rootdir}etc/pkg/"
 cp /usr/local/etc/pkg/repos/*.conf "${rootdir}usr/local/etc/pkg/repos/"
-pkg -r "${rootdir}" install -y pkg FreeBSD-runtime FreeBSD-caroot FreeBSD-certctl
+pkg -r "${rootdir}" install -y pkg FreeBSD-runtime FreeBSD-caroot FreeBSD-certctl FreeBSD-rc
 rm -rf "${rootdir}var/db/pkg/repos" "${rootdir}var/cache/pkg" "${rootdir}usr/local/sbin/pkg-static"
