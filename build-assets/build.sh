4f6cca94310803b310a2afb2903e3aca22478854
Frans-Jan van Steenbeek <frans-jan@van-steenbeek.net>
2025-06-11 17:02:22 +0200

    ANSI colours do not work

--- build-assets/build.sh
+++ build-assets/build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Sort dependencies
-echo -en "\e[34m"
+echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 echo " >> Sort dependencies"
-echo -en "\e[0m"
+echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 LIST=""
 for c in containers/*; do
     for d in $(grep FROM "${c}/Containerfile" | cut -d ' ' -f 2); do
@@ -14,34 +14,34 @@ for c in containers/*; do
 done
 
 # Build images in order
-echo -en "\e[34m"
+echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 echo " >> Build images"
-echo -en "\e[0m"
+echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 for c in $(echo -e "${LIST}" | tsort | tail -r); do
-    echo -en "\e[34m"
+    echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
     echo " >>> ${c}"
-    echo -en "\e[0m"
+    echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 
     # Some images need a non-containerized build
     mkdir -p "containers/${c}/root"
     if [ -x "containers/${c}/build.sh" ]; then
-        echo -en "\e[34m"
+        echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
         echo " >>>> run build.sh"
-        echo -en "\e[0m"
+        echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
         ./containers/${c}/build.sh
     fi
 
     # Build the image, package it as an archive
-    echo -en "\e[34m"
+    echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
     echo " >>>> buildah bud & push to archive"
-    echo -en "\e[0m"
+    echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
     buildah bud -t "${c}:latest" "./containers/${c}/Containerfile"
     buildah push "${c}:latest" "oci-archive:${c}.${1}.tar:${c}:latest"
 
     # Create image-specific tags
-    echo -en "\e[34m"
+    echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
     echo " >>>> construct tags and labels"
-    echo -en "\e[0m"
+    echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
     if [ -x "containers/${c}/info.sh" ]; then
         "./containers/${c}/info.sh" "${c}.${1}"
     else
@@ -50,16 +50,16 @@ for c in $(echo -e "${LIST}" | tsort | tail -r); do
     fi
 
     # Clean up (so the runner doesn't exit with an error)
-    echo -en "\e[34m"
+    echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
     echo " >>>> cleanup"
-    echo -en "\e[0m"
+    echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
     chflags -R noschg "containers/${c}/root" && rm -rf "containers/${c}/root"
 
     # If this is our core builder image, have a local version with pkg cache
     if [ "${c}" == "runtime-pkg" ]; then
-        echo -en "\e[34m"
+        echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
         echo " >>>> set up runtime-pkg as cached builder"
-        echo -en "\e[0m"
+        echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
         buildah from --name builder runtime-pkg
         buildah run builder pkg install -y FreeBSD-utilities
         buildah commit builder runtime-pkg
--- build-assets/setup.sh
+++ build-assets/setup.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
 # Set up PkgBase, to be used for container's root
-echo -en "\e[34m"
+echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 echo " >> Set up PkgBase"
-echo -en "\e[0m"
+echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 mkdir -p /usr/local/etc/pkg/repos
 cp build-assets/base.conf /usr/local/etc/pkg/repos/base.conf
 
 # Install OCIJail container runtime + Red Hat's container tooling
 # We primarily need buildah, but let's have the whole suite
-echo -en "\e[34m"
+echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 echo " >> Update and install podman-suite"
-echo -en "\e[0m"
+echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 pkg update
 pkg upgrade -y
 pkg install -y podman-suite
 
 # Set up PF for networking
-echo -en "\e[34m"
+echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 echo " >> Set up PF"
-echo -en "\e[0m"
+echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 cp /usr/local/etc/containers/pf.conf.sample /etc/pf.conf
 sed -i '' 's/ix0/vtnet0/' /etc/pf.conf
 echo -e "net.ip.forwarding=1\nnet.pf.filter_local=1" >> /etc/sysctl.conf
@@ -29,15 +29,15 @@ sysctl net.inet.ip.forwarding=1
 sysctl net.pf.filter_local=1
 
 # The runner is on UFS
-echo -en "\e[34m"
+echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 echo " >> Set up storage driver"
-echo -en "\e[0m"
+echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 sed -i '' -e 's/driver = "zfs"/driver = "vfs"/' /usr/local/etc/containers/storage.conf
 
 # Set Podman's networking interfaces' MTU to 16834 (same as lo0)
 # The default MTU leads to extremely slow networking due to fragmenting
-echo -en "\e[34m"
+echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 echo " >> Set up CNI MTU"
-echo -en "\e[0m"
+echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 mkdir -p /usr/local/etc/cni/net.d/
 cp build-assets/podman-bridge.conflist /usr/local/etc/cni/net.d/
