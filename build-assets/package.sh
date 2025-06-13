8a9765cc5df5d67a7a69ac245d5722b42febe792
Frans-Jan van Steenbeek <frans-jan@van-steenbeek.net>
2025-06-13 21:02:47 +0200

    Create manifests with modern buildah
    GHA gives us Ubuntu 24.04. Ubuntu gives us Buildah 1.33. We want
    something more modern.
    Buildah's stable image at quay.io is based on Fedora and is subsequently
    huge. We rather build our own.
    
    We share the local container storage with the containerized buildah so a
    lot of moving parts interact in a most unholy manner.

--- build-assets/package.sh
+++ build-assets/package.sh
@@ -11,21 +11,29 @@ echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 buildah manifest create ${1}:latest oci-archive:${1}.x86-64.tar oci-archive:${1}.arm64.tar
 
 echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-echo " >> Annotate manifest (license, source, description)"
+echo " > Give us a modern buildah"
 echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-for digest in $(buildah inspect ${1}:latest | jq -r '.manifests[].digest'); do
-    buildah manifest annotate \
-        --annotation "org.opencontainers.image.licenses=$(yq -r ".licenses[0]" ${1}.x86-64.yml)" \
-        --annotation "org.opencontainers.image.description=$(yq -r ".comment" ${1}.x86-64.yml)" \
-        --annotation "org.opencontainers.image.source=$(yq -r ".source" ${1}.x86-64.yml)" \
-        ${1}:latest "${digest}"
-done
+buildah from --name buildahbud alpine
+buildah run buildahbud apk add buildah
+buildah commit buildahbud buildah
+podman run -d --name buildah --security-opt label=disable --security-opt seccomp=unconfined --device /dev/fuse:rw \
+    -v ${HOME}/.local/share/containers:/var/lib/containers:Z -v .:/mnt buildah tail -f /dev/null
+
+echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+echo " > Create and annotate manifest"
+echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+podman exec buildah buildah manifest create ${1}:latest oci-archive:/mnt/${1}.x86-64.tar oci-archive:/mnt/${1}.arm64.tar
+podman exec buildah buildah manifest annotate --index \
+    --annotation "org.opencontainers.image.licenses=$(yq -r ".licenses[0]" ${1}.x86-64.yml)" \
+    --annotation "org.opencontainers.image.description=$(yq -r ".comment" ${1}.x86-64.yml)" \
+    --annotation "org.opencontainers.image.source=$(yq -r ".source" ${1}.x86-64.yml)" \
+    ${1}:latest
 
 echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 echo " >> Tag manifest"
 echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 for tag in $(cat ${1}.x86-64.tags) $(cat ${1}.arm64.tags); do
-    buildah tag ${1}:latest "${1}:${tag}"
+    podman exec buildah buildah tag ${1}:latest "${1}:${tag}"
 done
 
 echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
